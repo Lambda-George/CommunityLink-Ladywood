5223163ea17bfb50b48b9bc5df94d51d3ad24b17
George <khz23fdu@uea.ac.uk>
2026-06-07 14:42:58 +0100

    Services page

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 // middleware
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(
   session({
